7d900ac5796fbf58b51a6b7e0dcc07fc73d01d43
pvdo <fire_ra@bk.ru>
2025-01-12 12:20:38 +0300

    cards added to main screen

--- composeApp/src/commonMain/kotlin/com/vk/directop/App.kt
+++ composeApp/src/commonMain/kotlin/com/vk/directop/App.kt
@@ -4,12 +4,16 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.vk.directop.coin_detail.components.InfoCard
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -25,6 +29,16 @@ fun App() {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
+            InfoCard(
+                title = "Price",
+                formattedText = "$ 63,157.44",
+                icon = Icons.Filled.Info
+            )
+            InfoCard(
+                title = "Market capitalization",
+                formattedText = "$ 823,157.44",
+                icon = Icons.Filled.Done
+            )
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
